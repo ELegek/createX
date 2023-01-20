16928eae1779d7d41e372ac285c577c4d9b786d6
Evgeny Lezhepekov <evgeny.legek@yandex.ru>
2023-01-20 22:04:16 +0300

    сверстал блок с новостями

--- src/js/components/circular-progress.js
+++ src/js/components/circular-progress.js
@@ -1,89 +1,16 @@
-// let circularProgress = document.querySelector(".circular-progress");
-// let progressValue = document.querySelector(".progress-value");
-// let progressStartValue = 0;
-// let progressEndValue = 98;
-// let speed = 10;
-
-// let progress = setInterval(() => {
-//   progressStartValue++;
-//   progressValue.textContent = `${progressStartValue}%`;
-//   circularProgress.style.background = `conic-gradient(#F89828 ${
-//     progressStartValue * 3.6
-//   }deg, rgba(248, 152, 40, 0.6) 0deg)`;
-
-//   if (progressStartValue == progressEndValue) {
-//     clearInterval(progress);
-//   }
-// }, speed);
-
-// let circularProgressYears = document.querySelector(".circular-progress__years");
-// let progressValueYears = document.querySelector(".progress-value__years");
-// let progressStartValueYears = 0;
-// let progressEndValueYears = 20;
-// let speedYears = 10;
-
-// let progressYears = setInterval(() => {
-//   progressStartValueYears++;
-//   progressValueYears.textContent = `${progressStartValueYears}`;
-//   circularProgressYears.style.background = `conic-gradient(#f52f6e ${
-//     progressStartValueYears * 3.6
-//   }deg, rgba(245, 47, 110, 0.6) 0deg)`;
-
-//   if (progressStartValueYears == progressEndValueYears) {
-//     clearInterval(progressYears);
-//   }
-// }, speedYears);
-
-// let circularProgressHours = document.querySelector(".circular-progress__hours");
-// let progressValueHours = document.querySelector(".progress-value__hours");
-// let progressStartValueHours = 0;
-// let progressEndValueHours = 9452;
-// let speedHours = 10;
-
-// let progressHours = setInterval(() => {
-//   progressStartValueHours += 94;
-//   progressValueHours.textContent = `${progressStartValueHours}`;
-//   circularProgressHours.style.background = `conic-gradient(#5a87fc ${
-//     progressStartValueHours * 0.036
-//   }deg, rgba(90, 135, 252, 0.6) 0deg)`;
-
-//   if (progressStartValueHours >= progressEndValueHours) {
-//     clearInterval(progressHours);
-//   }
-// }, speedHours);
-
-// let circularProgressProjects = document.querySelector(
-//   ".circular-progress__projects"
-// );
-// let progressValueProjects = document.querySelector(".progress-value__projects");
-// let progressStartValueProjects = 0;
-// let progressEndValueProjects = 100;
-// let speedProjects = 10;
-
-// let progressProjects = setInterval(() => {
-//   progressStartValueProjects++;
-//   progressValueProjects.textContent = `${progressStartValueProjects}%`;
-//   circularProgressProjects.style.background = `conic-gradient(#03cea4 ${
-//     progressStartValueProjects * 3.6
-//   }deg, rgba(3, 206, 164, 0.6) 0deg)`;
-
-//   if (progressStartValueProjects == progressEndValueProjects) {
-//     clearInterval(progressProjects);
-//   }
-// }, speedProjects);
-
 import { Circle } from "progressbar.js";
 
 var circleBar1 = new Circle("#circular-1", {
-  color: "#000",
-  strokeWidth: 2,
-  trailWidth: 2,
-  trailColor: "#eee",
+  color: "#F89828",
+  strokeWidth: 6,
+  trailWidth: 6,
+  trailColor: "rgba(248, 152, 40, 0.6)",
   easing: "linear",
   text: {
     value: "0",
     className: "progress-text",
     style: {
+      color: "#000",
       position: "absolute",
       top: "50%",
       left: "50%",
@@ -97,15 +24,16 @@ var circleBar1 = new Circle("#circular-1", {
   },
 });
 var circleBar2 = new Circle("#circular-2", {
-  color: "#000",
-  strokeWidth: 2,
-  trailWidth: 2,
-  trailColor: "#eee",
+  color: "#F52F6E",
+  strokeWidth: 6,
+  trailWidth: 6,
+  trailColor: "rgba(245, 47, 110, 0.6)",
   easing: "linear",
   text: {
     value: "0",
     className: "progress-text",
     style: {
+      color: "#000",
       position: "absolute",
       top: "50%",
       left: "50%",
@@ -115,19 +43,20 @@ var circleBar2 = new Circle("#circular-2", {
     },
   },
   step: (state, shape) => {
-    shape.setText(Math.round(shape.value() * 100));
+    shape.setText(Math.round(shape.value() * 26.6));
   },
 });
 var circleBar3 = new Circle("#circular-3", {
-  color: "#000",
-  strokeWidth: 2,
-  trailWidth: 2,
-  trailColor: "#eee",
+  color: "#5A87FC",
+  strokeWidth: 6,
+  trailWidth: 6,
+  trailColor: "rgba(90, 135, 252, 0.6)",
   easing: "linear",
   text: {
     value: "0",
     className: "progress-text",
     style: {
+      color: "#000",
       position: "absolute",
       top: "50%",
       left: "50%",
@@ -137,19 +66,20 @@ var circleBar3 = new Circle("#circular-3", {
     },
   },
   step: (state, shape) => {
-    shape.setText(Math.round(shape.value() * 100));
+    shape.setText(Math.round(shape.value() * 11815));
   },
 });
 var circleBar4 = new Circle("#circular-4", {
-  color: "#000",
-  strokeWidth: 2,
-  trailWidth: 2,
-  trailColor: "#eee",
+  color: "#03CEA4",
+  strokeWidth: 6,
+  trailWidth: 6,
+  trailColor: "rgba(3, 206, 164, 0.6)",
   easing: "linear",
   text: {
     value: "0",
     className: "progress-text",
     style: {
+      color: "#000",
       position: "absolute",
       top: "50%",
       left: "50%",
@@ -159,7 +89,7 @@ var circleBar4 = new Circle("#circular-4", {
     },
   },
   step: (state, shape) => {
-    shape.setText(Math.round(shape.value() * 100));
+    shape.setText(Math.round(shape.value() * 100) + "%");
   },
 });
 
@@ -171,13 +101,13 @@ var visible = function (e) {
   // Если положение нижней части окна больше чем верхняя координата элемента, то мы долистали до элемента
   // Так же проверим, не запущена ли уже анимация, а то выйдет казус в виде всё время запускающееся анимации
   if (elementTop < windowBottom && Number(e.dataset.animationState) === 0) {
-    circleBar1.animate(1, {
+    circleBar1.animate(0.98, {
       duration: 2000,
     });
-    circleBar2.animate(1, {
+    circleBar2.animate(0.75, {
       duration: 2000,
     });
-    circleBar3.animate(1, {
+    circleBar3.animate(0.8, {
       duration: 2000,
     });
     circleBar4.animate(1, {
